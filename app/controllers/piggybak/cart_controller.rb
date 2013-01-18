1763c30111b8b4e1e63c5f03d83fc7ed7e913437
vala <gonjo@free.fr>
2013-01-18 19:03:47 +0100

    allow passing options to cart_form, and make it posible to retrieve json cart if needed

--- app/controllers/piggybak/cart_controller.rb
+++ app/controllers/piggybak/cart_controller.rb
@@ -4,23 +4,27 @@ module Piggybak
       @cart = Cart.new(cookies["cart"])
       @cart.update_quantities
       cookies["cart"] = { :value => @cart.to_cookie, :path => '/' }
+      respond_to do |format|
+        format.html
+        format.json { render json: @cart.to_json }
+      end
     end
-  
+
     def add
       cookies["cart"] = { :value => Cart.add(cookies["cart"], params), :path => '/' }
       redirect_to piggybak.cart_url
     end
-  
+
     def remove
       response.set_cookie("cart", { :value => Cart.remove(cookies["cart"], params[:item]), :path => '/' })
       redirect_to piggybak.cart_url
     end
-  
+
     def clear
       cookies["cart"] = { :value => '', :path => '/' }
       redirect_to piggybak.cart_url
     end
-  
+
     def update
       cookies["cart"] = { :value => Cart.update(cookies["cart"], params), :path => '/' }
       redirect_to piggybak.cart_url
